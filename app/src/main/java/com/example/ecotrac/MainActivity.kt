1f64571906ed4398ebafe5b547bd1cb77780b73a
Savio Soares <dsaviosnogueira@gmail.com>
2023-12-08 15:14:44 -0300

    Created header screen main

--- app/src/main/java/com/example/ecotrac/MainActivity.kt
+++ app/src/main/java/com/example/ecotrac/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.ecotrac
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ecotrac.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var binding : ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        val inputSearch = binding.editSearch
+        inputSearch.setTextColor(Color.BLACK)
     }
 }
