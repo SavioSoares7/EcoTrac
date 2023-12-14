dcd0237156c632c85e076eeca90d77ffc28d4fc6
Savio Soares <dsaviosnogueira@gmail.com>
2023-12-14 00:11:32 -0300

    Finished Routes pages register and login & Create fail input

--- app/src/main/java/com/example/ecotrac/LoginActivity.kt
+++ app/src/main/java/com/example/ecotrac/LoginActivity.kt
@@ -1,11 +1,28 @@
 package com.example.ecotrac
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ecotrac.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        // Use o View Binding para inflar o layout
+        val binding: ActivityLoginBinding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)  // Defina a view inflada como o conteúdo da atividade
+
+        val textRegister = binding.textRegister
+
+        textRegister.setOnClickListener {
+            toEnterScreenRegister()
+        }
+    }
+
+    private fun toEnterScreenRegister() {
+        val intent = Intent(this, singup::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/ecotrac/singup.kt
+++ app/src/main/java/com/example/ecotrac/singup.kt
@@ -1,11 +1,54 @@
 package com.example.ecotrac
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import com.example.ecotrac.databinding.ActivityMainBinding
+import com.example.ecotrac.databinding.ActivitySingupBinding
 
 class singup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_singup)
+
+        var binding : ActivitySingupBinding = ActivitySingupBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        val textToEnter = binding.textToEnter
+        val inputEmail = binding.inputEmail
+        val inputPassword = binding.inputPassword
+        val buttonRegister = binding.buttonRegister
+
+
+        buttonRegister.setOnClickListener{
+            registerUser(inputEmail,inputPassword)
+        }
+
+        textToEnter.setOnClickListener{
+            toEnterScreenLogin()
+        }
+
+    }
+
+    private fun toEnterScreenLogin(){
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun registerUser(emailField : EditText, passwordField : EditText){
+        val email = emailField.text.toString().trim()
+        val password = passwordField.text.toString().trim()
+
+        if(email.isEmpty() || password.isEmpty()){
+            Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_LONG).show()
+
+            emailField.setBackgroundResource(R.drawable.edit_linear_input_fail)
+            passwordField.setBackgroundResource(R.drawable.edit_linear_input_fail)
+            return
+        }
+
     }
 }
