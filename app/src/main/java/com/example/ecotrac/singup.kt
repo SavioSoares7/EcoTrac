c98e85343209b9725a3b9034a46835fb662fdb4d
Savio Soares <dsaviosnogueira@gmail.com>
2023-12-13 21:28:26 -0300

    Create screen singup

---
+++ app/src/main/java/com/example/ecotrac/singup.kt
@@ -0,0 +1,11 @@
+package com.example.ecotrac
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class singup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_singup)
+    }
+}
